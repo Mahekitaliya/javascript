6246fe94596a9d75ca98c76bb6fa67a221f606c2
mahek <mahekitaliya2005@gmail.cpm>
2024-02-21 08:17:22 +0530

    frist

--- function/2.js
+++ function/2.js
@@ -1,23 +1,23 @@
 //basic function ==
 
-function add(a, b){
-    return a+b;
-}
+// function add(a, b){
+//     return a+b;
+// }
 
-console.log(add(10,15));
-let z = add(10 , 15);
-console.log(z);
+// console.log(add(10,15));
+// let z = add(10 , 15);
+// console.log(z);
 
 
 
 //anonymous function / function as exprestion ==
 
 
-let prouct = function(x , y){
-    return x*y;
-}
+// let prouct = function(x , y){
+//     return x*y;
+// }
 
-console.log(prouct(12 , 16));
+// console.log(prouct(12 , 16));
 
 
 //IIFE ==
@@ -30,49 +30,89 @@ console.log(prouct(12 , 16));
 // })();
 
 
-let x = (function hello(a , b){
-    return (a+ b);
+// let x = (function hello(a , b){
+//     return (a+ b);
 
-})(10 , 15);
-console.log(x);
-
-
-
-let y = (function hello(a , b){
-    return (a+ b);
-
-})("10" , 15);
-console.log(y);
+// })(10 , 15);
+// console.log(x);
 
 
 
-let u = (function hello(a , b){
-    return (a+ b);
+// let y = (function hello(a , b){
+//     return (a+ b);
 
-})("10" , "15");
-console.log(u);
+// })("10" , 15);
+// console.log(y);
+
+
+
+// let u = (function hello(a , b){
+//     return (a+ b);
+
+// })("10" , "15");
+// console.log(u);
 
 
 // arrow function
 
-let add = (a, b) => {
-    return a+b;
-}
+// let add = (a, b) => {
+//     return a+b;
+// }
 
 
 //  arrow  single line statement
 
-let add = (a , b) => a+b;
-console.log(12,65);
+// let add = (a , b) => a+b;
+// console.log(12,65);
 
 
 
 // function constructor
 
-let hello = newmfunction("a" , "b" , "return a/b");
-console.log(hello(12,13));
+// let hello = newmfunction("a" , "b" , "return a/b");
+// console.log(hello(12,13));
+
+
+// Function are Object
+// function myFunction(a,b) {
+//     return arguments.length; 
+// }
+
+// let text = myFunction(4,3);
+// console.log(text);
+
+
+// function myFunction(a, b) {
+//     return a * b;
+// }
+
+// let text = myFunction(4,3).toString();  
+
+// console.log(text);
 
 
 
+// function Rest Parameters
+
+// function sum(...args) {
+//     let sum = 0;
+// for (let arg of args) 
+//                 sum += arg;
+// return sum;
+// }
+
+// let x = sum(4, 9);
+// console.log(x);
+
+
+function add(...args){
+    let sum = 0;
+    for(let i= 0 ; i<args.length ; i++)
+        sum = sum + args[i];
+
+        return sum;
+}
+
+console.log(add(16, 16));
 
 
