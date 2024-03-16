1e3d0746a68dbc1acd5dc3f51b605a77bad95165
mahek <mahekitaliya2005@gmail.cpm>
2024-03-16 07:04:26 +0530

    frist

---
+++ DOM-method/index.js
@@ -0,0 +1,64 @@
+// javasript getelementByid----------
+
+// function findelement(){
+//     const element  = document.getElementById("id1");
+//     element.innerHTML = "element found........!";
+//     element.style.color = "red";
+// }
+
+
+
+
+
+
+// {
+// const elements = document.getElementsByTagName("p");
+// console.log(elements); 
+//     function changeColor(){
+//         for(let i = 0; i < elements.length; i++){
+//             if(i==2){
+//                 elements[i].style.color = "red";
+//                 elements[i].style.border = "1px solid green";
+//             }else{
+//                 elements[i].style.color = "blue";
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function message()
+//  {
+//     confirm("yes i am h1 tag")
+//     // alert("yes i am h1 tag")
+//  }
+
+//  function dateshow()
+//  {
+//     Document.getElementsById("date").innerHTML = date();
+//  }
