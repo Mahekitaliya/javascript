2fb010bc0ca42a2fcb072180f7d2f8bb63f25f35
mahek <mahekitaliya2005@gmail.cpm>
2024-03-20 08:26:53 +0530

    frist

--- loop/1.js
+++ loop/1.js
@@ -1,15 +1,15 @@
 // Print a pattern using a for loop
 
-{
-    let i,j;
-    for(j=1; j<=3 ;j++)
-        {
-            for(i=1 ; i<=5; i++);
-        }
-        document.write("*");
-    {
+// {
+//     let i,j;
+//     for(j=1; j<=3 ;j++)
+//         {
+//             for(i=1 ; i<=5; i++);
+//         }
+//         document.write("*");
+//     {
         
-        document.write("<br>");
-    }
-}
+//         document.write("<br>");
+//     }
+// }
 
